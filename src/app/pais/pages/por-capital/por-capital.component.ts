6702d19e0651f31c30b0f2dee0a87f72f46708e3
Guillermo Villagomez <gvdelamora@gmail.com>
2022-01-19 07:34:32 -0600

    Por-pais done

---
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-capital',
+  templateUrl: './por-capital.component.html',
+  styles: [
+  ]
+})
+export class PorCapitalComponent {
+  
+  termino : string = '';
+  hayError: boolean = false;
+  paises  : Country[] = []
+
+  constructor( private paisService: PaisService) { }
+
+  buscar( termino: string ){
+
+    this.hayError= false;
+    this.termino = termino;
+
+    this.paisService.buscarPorCapital( termino )
+    .subscribe({
+      next: paises =>{
+        this.paises = paises;
+        console.log(paises)
+      },
+      error: err => {
+        this.hayError = true;
+        this.paises = []
+        console.info(err)
+      }
+    })
+
+    
+  }
+    sugerencias( termino: string ){
+          this.hayError = false;
+    }
+}
---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [
+  ]
+})
+export class PorPaisComponent  {
+  
+  termino : string = '';
+  hayError: boolean = false;
+  paises  : Country[] = []
+
+  constructor( private paisService: PaisService) { }
+
+  buscar( termino: string ){
+
+    this.hayError= false;
+    this.termino = termino;
+
+    this.paisService.buscarPais( termino )
+    .subscribe({
+      next: paises =>{
+        this.paises = paises;
+        console.log(paises)
+      },
+      error: err => {
+        this.hayError = true;
+        this.paises = []
+        console.info(err)
+      }
+    })
+
+    
+  }
+    sugerencias( termino: string ){
+          this.hayError = false;
+    }
+}
